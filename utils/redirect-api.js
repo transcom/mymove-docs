0be086eda0b4987ed999d1fa7692d0b903057721
Roger Steve Ruiz <rogeruiz@truss.works>
2021-10-30 01:10:53 -0400

    Move all redirects to their own files

---
+++ utils/redirect-api.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    to: '/docs/api/guides/api-errors',
+    from: [
+      '/docs/dev/contributing/backend/API-Errors',
+    ],
+  },
+  {
+    to: '/docs/api/guides/api-programming-guide',
+    from: [
+      '/docs/dev/contributing/backend/API-Programming-Guide',
+    ],
+  },
+  {
+    to: '/docs/api/guides/api-style-guide',
+    from: [
+      '/docs/dev/contributing/backend/API-Style-Guide',
+    ],
+  },
+  {
+    to: '/docs/api/guides/guide-to-creating-an-endpoint',
+    from: [
+      '/docs/dev/contributing/backend/Guide-to-Creating-an-Endpoint',
+    ],
+  },
+  {
+    to: '/docs/api/guides/how-to-deprecate-endpoints',
+    from: [
+      '/docs/dev/versioning/How-to-deprecate-endpoints',
+    ],
+  },
+  {
+    to: '/docs/api/testing/acceptance-testing-prime-api-endpoints',
+    from: [
+      '/docs/dev/testing/running-tests/Acceptance-Testing-Prime-API-Endpoints',
+    ],
+  },
+  {
+    to: '/docs/api/testing/end-to-end-testing-playing-the-prime',
+    from: [
+      '/docs/dev/testing/running-tests/End-to-End-Testing-Playing-the-Prime',
+    ],
+  },
+  {
+    to: '/docs/api/testing/how-to-test-the-prime-api',
+    from: [
+      '/docs/dev/getting-started/How-to-Test-the-Prime-API',
+    ],
+  },
+  {
+    to: '/docs/api/docs/push-notifications-to-prime',
+    from: [
+      '/docs/dev/contributing/backend/Push-Notifications-to-Prime',
+    ],
+  },
+];
