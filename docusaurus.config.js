d1b71a55838dcd24e488d0bdd8483033aa07cf12
Jackson Rosenthal <jackson.rosenthal@caci.com>
2023-08-04 15:19:55 +0000

    updated references and titles

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const APIPages = require('./utils/redirect-api');
 module.exports = {
   title: 'MilMove Developer Portal',
   tagline: '',
-  url: 'https://transcom.github.io',
+  url: 'https://caci-milmove.github.io',
   baseUrl: '/mymove-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
@@ -81,7 +81,7 @@ module.exports = {
           label: 'Help',
         },
         {
-          href: 'https://github.com/transcom/mymove',
+          href: 'https://github.com/caci-milmove/mymove',
           label: 'mymove',
           position: 'right',
         },
@@ -129,11 +129,11 @@ module.exports = {
           items: [
             {
               label: 'MilMove Documentation GitHub',
-              href: 'https://github.com/transcom/mymove-docs',
+              href: 'https://github.com/caci-milmove/mymove-docs',
             },
             {
               label: 'MilMove GitHub',
-              href: 'https://github.com/transcom/mymove',
+              href: 'https://github.com/caci-milmove/mymove',
             },
             {
               label: 'Docusaurus Official Docs',
@@ -155,7 +155,7 @@ module.exports = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/transcom/mymove-docs/edit/main/',
+          editUrl: 'https://github.com/caci-milmove/mymove-docs/edit/main/',
           remarkPlugins: [require('mdx-mermaid')],
         },
         theme: {
@@ -168,32 +168,32 @@ module.exports = {
       {
         specs: [
           {
-            spec: 'https://raw.githubusercontent.com/transcom/mymove/main/swagger/admin.yaml',
+            spec: 'https://raw.githubusercontent.com/caci-milmove/mymove/main/swagger/admin.yaml',
             route: '/api/admin',
           },
           {
-            spec: 'https://raw.githubusercontent.com/transcom/mymove/main/swagger/internal.yaml',
+            spec: 'https://raw.githubusercontent.com/caci-milmove/mymove/main/swagger/internal.yaml',
             route: '/api/internal',
           },
           {
-            spec: 'https://raw.githubusercontent.com/transcom/mymove/main/swagger/ghc.yaml',
+            spec: 'https://raw.githubusercontent.com/caci-milmove/mymove/main/swagger/ghc.yaml',
             route: '/api/ghc',
           },
           // Continue to support existing url for those that have bookmarked this link. We can route this to the oldest supported definition
           {
-            spec: 'https://raw.githubusercontent.com/transcom/mymove/main/swagger/prime.yaml',
+            spec: 'https://raw.githubusercontent.com/caci-milmove/mymove/main/swagger/prime.yaml',
             route: '/api/prime',
           },
           {
-            spec: 'https://raw.githubusercontent.com/transcom/mymove/main/swagger/prime.yaml',
+            spec: 'https://raw.githubusercontent.com/caci-milmove/mymove/main/swagger/prime.yaml',
             route: '/api/prime/v1',
           },
           {
-            spec: 'https://raw.githubusercontent.com/transcom/mymove/main/swagger/prime_v2.yaml',
+            spec: 'https://raw.githubusercontent.com/caci-milmove/mymove/main/swagger/prime_v2.yaml',
             route: '/api/prime/v2',
           },
           {
-            spec: 'https://raw.githubusercontent.com/transcom/mymove/main/swagger/support.yaml',
+            spec: 'https://raw.githubusercontent.com/caci-milmove/mymove/main/swagger/support.yaml',
             route: '/api/support',
           },
         ],
